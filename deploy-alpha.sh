a9b38674bc17b0334a0f12bed6cf3df0f0416c5f
Hwan Jo <nimbusob@gmail.com>
2025-12-15 17:20:29 +0900

    Github workflow 테스트용

---
+++ deploy-alpha.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Alpha deployment 를 담당하는 Github workflows (이름: Alpha 배포, file: .github/workflows/deploy-alpha.yml) 을 trigger 합니다.
+# 참조 URL: https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28
+
+echo "Work in progress"
